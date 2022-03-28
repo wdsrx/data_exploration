eca7a56467cd2486f3a2b7b5f88a50a01482fa9b
Julio Meza <juliomeza@gmail.com>
2022-03-28 18:54:36 -0400

    Compacting the code

--- js/plots.js
+++ js/plots.js
@@ -132,7 +132,6 @@ function openOutbounds() {
 openOutbounds()
 
 // PICKING //
-
 // All warehouses
 function pickers() {
   d3.json('data/pickers.json').then((data) => {
@@ -184,7 +183,7 @@ function pickers() {
 }
 pickers()
 
-function pickers_w10(warehouse) {
+function pickers_w(warehouse, place) {
   d3.json(warehouse).then((data) => {
     //console.log(data);
 
@@ -223,53 +222,54 @@ function pickers_w10(warehouse) {
         text: 'WAREHOUSE 10 - Picking by Employee'
       }
     }
-    Plotly.newPlot('plot_4_1', chunks, layout)
+    Plotly.newPlot(place, chunks, layout)
   })
 }
-pickers_w10('data/pickers_w10.json')
+pickers_w('data/pickers_w10.json', 'plot_4_1')
+pickers_w('data/pickers_w20.json', 'plot_4_2')
 
-function pickers_w20() {
-  d3.json('data/pickers_w20.json').then((data) => {
-    //console.log(data);
+// function pickers_w20() {
+//   d3.json('data/pickers_w20.json').then((data) => {
+//     //console.log(data);
 
-    // Hours
-    first = Object.values(data)[0]
-    var hours = Object.keys(first)
-    //console.log(hours)
-    // console.log(Object.values(a))
+//     // Hours
+//     first = Object.values(data)[0]
+//     var hours = Object.keys(first)
+//     //console.log(hours)
+//     // console.log(Object.values(a))
 
-    // Employees (x)
-    var labels = Object.keys(data)
-    //console.log(labels)
+//     // Employees (x)
+//     var labels = Object.keys(data)
+//     //console.log(labels)
 
-    var dataSize = hours.length
-    var chunks = []
+//     var dataSize = hours.length
+//     var chunks = []
     
-    for (var i = 0; i < dataSize; i++) {
-      // Picking Activity (y)
-      var picks = []
-      Object.values(data).forEach((line) => {
-        //console.log(Object.keys(line)[i])
-        //console.log(Object.values(line)[i])
-        picks.push(Object.values(line)[i])
-      })
-      var trace = {
-        x: labels,
-        y: picks,
-        name: hours[i],
-        type: 'bar'
-      }
-      chunks.push(trace)
-    }
-    var layout = {
-      barmode: 'stack',
-      title: {
-        text: 'WAREHOUSE 20 -  Picking by Employee'
-      }
-    }
-    Plotly.newPlot('plot_4_2', chunks, layout)
-  })
-}
-pickers_w20()
+//     for (var i = 0; i < dataSize; i++) {
+//       // Picking Activity (y)
+//       var picks = []
+//       Object.values(data).forEach((line) => {
+//         //console.log(Object.keys(line)[i])
+//         //console.log(Object.values(line)[i])
+//         picks.push(Object.values(line)[i])
+//       })
+//       var trace = {
+//         x: labels,
+//         y: picks,
+//         name: hours[i],
+//         type: 'bar'
+//       }
+//       chunks.push(trace)
+//     }
+//     var layout = {
+//       barmode: 'stack',
+//       title: {
+//         text: 'WAREHOUSE 20 -  Picking by Employee'
+//       }
+//     }
+//     Plotly.newPlot('plot_4_2', chunks, layout)
+//   })
+// }
+// pickers_w20()
 
 
