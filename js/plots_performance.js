b9eb5485be2f8d3a40901da341cb2a3289f9699a
Julio Meza <juliomeza@gmail.com>
2022-04-01 16:18:27 -0400

    Updating data

--- js/plots_performance.js
+++ js/plots_performance.js
@@ -1,6 +1,9 @@
 function performance() {
     d3.json('data/performance.json').then ((data) => {
         console.log(data)
+        console.log(Object.keys(data).length)
+        console.log(Object.values(data).length)
+        
     })
 }
 performance()
